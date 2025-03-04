e6950b1dc398c8771fdcc926d7f3763ed37a506d
Mohammed lnagar <35522526+mohammedlnagar@users.noreply.github.com>
2025-03-04 13:14:18 +0400

    new

--- wasel/settings.py
+++ wasel/settings.py
@@ -163,7 +163,7 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "wasel/static")]
 
 # Default primary key field type
